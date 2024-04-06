d4531c4c856821f73bf4d7e7c9b97c3e8a51074e
Maxwell Olmen <mathematickid@gmail.com>
2024-04-05 21:30:35 -0500

    HOTFIX - forgot to remove GameCommand refs

--- src/main/java/fr/cermak/gamesuite/controller/GameSuiteController.java
+++ src/main/java/fr/cermak/gamesuite/controller/GameSuiteController.java
@@ -2,14 +2,13 @@ package fr.cermak.gamesuite.controller;
 
 import fr.cermak.gamesuite.GameSuite;
 import fr.cermak.gamesuite.socket.ClientHandler;
-import fr.cermak.gamesuite.socket.GameCommand;
 import fr.cermak.gamesuite.socket.GameResponse;
 import fr.cermak.gamesuite.util.MessageMapping;
 
 import java.io.IOException;
 
 public class GameSuiteController {
-    @MessageMapping(path = GameCommand.PING)
+    @MessageMapping(path = 0)
     public GameResponse pong(byte[] data) throws IOException {
         GameResponse response = new GameResponse(GameResponse.PONG, null);
         for (ClientHandler otherClient : GameSuite.handler.getClients()) {
